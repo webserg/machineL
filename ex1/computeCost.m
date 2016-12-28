4e47e4d22183e507233001eaa4caa9a1b9308aa0
webserg <webserg@gmail.com>
2016-12-28 14:29:23 +0200

    Computing Cost (for One Variable)

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -12,8 +12,10 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
+h = X*theta;
+delta = h-y;
+delta_square = delta.^2;
+J = sum(delta_square)/(2*m)
 
 
 
