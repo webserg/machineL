bd250f98fa243a65c3395f1d5a8a879bc944a443
webserg <webserg@gmail.com>
2016-12-29 00:36:17 +0200

    finish first hw

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,11 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    h = X*theta;
+    delta = alpha/m * sum((h-y).*X);
+    theta = theta' - delta;
+    theta = theta'
+    
 
 
     % ============================================================
