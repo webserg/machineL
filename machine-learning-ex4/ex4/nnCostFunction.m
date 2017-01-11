3265535d2af03d2ba417e1e94396fbe49edbc780
webserg <webserg@gmail.com>
2017-01-11 22:37:00 +0200

    final

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -101,7 +101,8 @@ for i = 1 : m
 end
 Theta1_grad=Theta1_grad./m;
 Theta2_grad=Theta2_grad./m;
-
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + ( lambda/m ) * Theta1(:,2:end);
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + ( lambda/m ) * Theta2(:,2:end);
 
 
 
